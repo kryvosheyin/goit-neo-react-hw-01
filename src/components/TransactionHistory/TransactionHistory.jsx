702ab2c1d34d082bb5768979a1695e6817e00583
kryvosheyin <alex.kryvosheyin@gmail.com>
2024-07-28 17:06:32 +0100

    hw completed

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,29 @@
+import style from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={style.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }, index) => {
+          return (
+            <tr key={id} className={index % 2 == 0 ? style.even : style.odd}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
